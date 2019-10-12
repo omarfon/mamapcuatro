76004eba145d00751eb2c32fde2e7c62f68e2f00
omar <fonseca>
2019-10-12 12:54:34 -0500

    actualizando servicios a prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,23 @@ export const environment = {
   production: true
 };
 
-/* export const API_ENDPOINT = "https://dappapache02.eastus.cloudapp.azure.com/middleware2-copy/api/v2/"; */
-export const API_ENDPOINT = "https://api.aviva.pe/middleware2/api/v2/";
-export const API_NOTAS = "https://api.aviva.pe/middleware2/api/v2/mama/notas";
+/* ENDPOINTS DESARROLLO */
+/* export const API_ENDPOINT = "https://dappapache02.eastus.cloudapp.azure.com/middleware2-copy/api/v2/";
+export const API_NOTAS = "https://dappapache02.eastus.cloudapp.azure.com/middleware2-copy/api/v2/mama/notas";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBMopx6KZ79wFANi2Yb6ysxvAIdRso96VY",
+  authDomain: "aviva-mamapp-dev.firebaseapp.com",
+  databaseURL: "https://aviva-mamapp-dev.firebaseio.com",
+  projectId: "aviva-mamapp-dev",
+  storageBucket: "aviva-mamapp-dev.appspot.com",
+  messagingSenderId: "519988913097",
+  appId: "1:519988913097:web:dca29f129d59cc0b"
+} */
+
+/* ENDPOINTS PRODUCCION */
+export const API_ENDPOINT = "https://api.aviva.pe/middleware2/api/v2/"; 
+export const API_NOTAS = "https://api.aviva.pe/middleware2/api/v2/mama/notas"; 
 
 export const firebaseConfig ={
   apiKey: "AIzaSyC79bO4W3NAW6tjRGe8Y-cx9bK2Nb5t760",
@@ -14,14 +28,6 @@ export const firebaseConfig ={
   storageBucket: "aviva-mamapp-prod.appspot.com",
   messagingSenderId: "123950719277",
   appId: "1:123950719277:web:331d4e07590df134be73a4"
-}
+} 
 
-/* export const firebaseConfig = {
-  apiKey: "AIzaSyBMopx6KZ79wFANi2Yb6ysxvAIdRso96VY",
-  authDomain: "aviva-mamapp-dev.firebaseapp.com",
-  databaseURL: "https://aviva-mamapp-dev.firebaseio.com",
-  projectId: "aviva-mamapp-dev",
-  storageBucket: "aviva-mamapp-dev.appspot.com",
-  messagingSenderId: "519988913097",
-  appId: "1:519988913097:web:dca29f129d59cc0b"
-} */
+
