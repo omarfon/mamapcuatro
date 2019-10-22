6586db1f0554aac7fa12513769ba4ebaa1feace6
omar <fonseca>
2019-10-22 10:58:02 -0500

    modificando login/ login recovery

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -12,7 +12,7 @@ export class UserService {
 
   private SERVER = API_ENDPOINT;
   /* private SERVER2 = "https://dappapache02.eastus.cloudapp.azure.com/middleware2-copy/api/v2/auth/login-firebase" */
-  private apiUrl = `${this.SERVER}auth/login`;
+  private apiUrl = `${this.SERVER}`;
   private apiUrlDatos = `${this.SERVER}auth/login/ebooking/`;
 
   constructor( public http: HttpClient) { }
@@ -22,7 +22,7 @@ export class UserService {
     let headers = new HttpHeaders({'Authorization': authorization});
 
     let params = {email: email, password: password, app:"mama"}
-      return this.http.post(this.apiUrl , params, {headers}).pipe( 
+      return this.http.post(`${this.SERVER}auth/login` , params, {headers}).pipe( 
               map(data =>{
               return data
         })
@@ -32,14 +32,14 @@ export class UserService {
   doSignInforNewRegister(email, password){
     let params = {email:email, password: password , captchaId: "4dbc9e7e-a921-4d1f-8e36-1021a670c47a"}
     // let params = {email:email, password: shajs('sha256').update(password).digest('hex') }
-    return this.http.post(this.apiUrl + 'login', params).pipe(
+    return this.http.post(`${this.apiUrl}auth/login`, params).pipe(
                   map(data =>{
                     return data
                   })
     )
   }
   doSignUp(params){
-    return this.http.post(this.apiUrl + 'register', params).pipe(
+    return this.http.post(`${this.apiUrl}users/register`, params).pipe(
                   map( data =>{
                     return data
                   })
@@ -65,7 +65,7 @@ export class UserService {
     let params = {code: datos.code, email: datos.email, id: datos.id, password: datos.password, app: 'ebooking'};
     const authorization = localStorage.getItem('authorization');
     let headers = new HttpHeaders({"Authorization": authorization});
-    return this.http.post(this.apiUrl + 'login-recovery', params, {headers}).pipe(
+    return this.http.post(`${this.apiUrl}users/login-recovery`, params, {headers}).pipe(
                     map(data => {
                       return data
                     })/* .catch(e =>{
@@ -79,7 +79,7 @@ export class UserService {
     // console.log('los paramasque cambian la contraseña:', params);
     const authorization = localStorage.getItem('authorization');
     let headers = new HttpHeaders({"Authorization": authorization});
-    return this.http.put(this.apiUrl + 'update-password', params, {headers}).pipe(
+    return this.http.put(`${this.apiUrl}users/update-password`, params, {headers}).pipe(
                     map(data => {
                       return data
                       
